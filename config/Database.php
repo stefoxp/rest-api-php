65ca4dd71efec4320997bf60dfa1e69104149420
StefoXP <stefano.pasquini.2010@gmail.com>
2023-03-31 12:42:45 +0200

    Db class

---
+++ config/Database.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Description of database
+ *
+ * @author stefano
+ */
+class Database {
+    // credentials
+    private $host = "localhost";
+    private $db_name = "rest-library";
+    private $username = "root";
+    private $password = "";
+    
+    public $conn;
+    
+    // db connection
+    public function getConnection()
+    {
+        $this->conn = null;
+        try
+        {
+            $this->conn = new PDO("mysql:host=" . $this->host 
+                    . ";dbname=" . $this->db_name,
+                    $this->username,
+                    $this->password);
+            $this->conn->exec("set names utf8");
+        }
+        catch (Exception $ex)
+        {
+            echo("Errore di connessione: " . $exception->getMessage());
+        }
+        return $this->conn;
+    }
+}
